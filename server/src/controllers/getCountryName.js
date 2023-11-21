df6c5c952b801c0b11c57bf8e9f215528c56c5ab
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2023-11-20 19:31:19 -0500

    back name

---
+++ server/src/controllers/getCountryName.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+const { Op } = require('sequelize');
+const {Country,Activity}= require("../db")
+
+const getName = async (req,res)=>{
+    const {nameB} = req.params;
+    try{
+        const response = await Country.findOne({where:{name:{ [Op.iLike]: nameB }}})
+        if(response){
+            const result = {
+                id:response.id,
+                name:response.name,
+                image: response.image,
+                continente: response.continente,
+                capital: response.capital,
+                subregion: response.subregion,
+                area: response.area,
+                poblacion: response.poblacion,
+               
+            }
+            console.log("yo")
+            return res.status(200).json(result);
+        }
+    }catch(error){
+        res.status(500).send(error.message);
+    }
+}
+
+module.exports={
+    getName
+}
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const {paises} = require ("../controllers/getAllCountries")
 const {getById} = require ("../controllers/getIdCountry.js")
+const {getName} = require ("../controllers/getCountryName.js")
+
 
 const router = Router();
 router.get('/countrys',paises);
 router.get('/countrys/:idKey',getById);
+router.get('/country/:nameB',getName);
 module.exports = router;
